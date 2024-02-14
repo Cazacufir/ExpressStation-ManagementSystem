ec44116156dac0b656e02529a514c26024cb1e00
Cazacufir <103994575+Cazacufir@users.noreply.github.com>
2024-02-15 00:42:20 +0800

    headerfix

--- Client/backstage/src/layout/compoents/header.vue
+++ Client/backstage/src/layout/compoents/header.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="header w-full h-full text-center bg-red-200 flex items-center justify-center">
-        <span class="title font-semibold text-white text-9xl">驿 站 管 理 系 统</span>
+        <span class="title font-semibold text-white text-9xlhead">驿 站 管 理 系 统</span>
     </div>
 </template>
 
