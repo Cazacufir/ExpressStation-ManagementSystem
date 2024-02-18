fc8ddb189d52df30a2f11c215c4fc6230f931588
Cazacufir <103994575+Cazacufir@users.noreply.github.com>
2024-02-19 01:10:32 +0800

    尝试登录

--- src/main/java/com/parcelhub/entity/Admin.java
+++ src/main/java/com/parcelhub/entity/Admin.java
@@ -2,6 +2,7 @@ package com.parcelhub.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,7 @@ import java.io.Serializable;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@TableName("admin")
 public class Admin implements Serializable {
 
     @TableId(type = IdType.AUTO)
--- src/main/java/com/parcelhub/service/impl/LoginServiceImpl.java
+++ src/main/java/com/parcelhub/service/impl/LoginServiceImpl.java
@@ -1,14 +1,29 @@
 package com.parcelhub.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.parcelhub.entity.Admin;
+import com.parcelhub.mapper.AdminMapper;
 import com.parcelhub.service.LoginService;
 import com.parcelhub.utils.Result;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class LoginServiceImpl implements LoginService {
 
+    @Autowired
+    private AdminMapper adminMapper;
+
     public Result login(Admin admin){
+        QueryWrapper<Admin> adminQueryWrapper = new QueryWrapper<>();
+        adminQueryWrapper.eq("contact",admin.getContact())
+                .eq("password",admin.getPassword());
+        List<Admin> user = adminMapper.selectList(adminQueryWrapper);
+        if(user.isEmpty()){
+            return Result.errorResult(401,"账号或者密码错误!");
+        }
         return Result.okResult();
     }
 }
