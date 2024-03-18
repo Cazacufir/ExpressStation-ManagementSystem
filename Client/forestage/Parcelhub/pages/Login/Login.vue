e87b4461086ff40690f0b35edafefaca892bf18f
Cazacufir <103994575+Cazacufir@users.noreply.github.com>
2024-03-18 17:40:18 +0800

    登录注册静态页面

---
+++ Client/forestage/Parcelhub/pages/Login/Login.vue
@@ -0,0 +1,87 @@
+<template>
+	<view class="container">
+		<div class="logincard">
+			<u-text text="欢迎登录!" bold="true" size="35"></u-text>
+			<u-form :model="user" :rules="loginRules" ref="LoginFormRef">
+				<u-form-item prop="contact">
+					<u-input v-model="user.contact" placeholder="请输入手机号" clearable
+					prefixIcon="phone" shape="circle" prefixIconStyle="font-size:30px"></u-input>
+				</u-form-item>
+				
+				<u-form-item prop="password">
+					<u-input v-model="user.password" placeholder="请输入密码" clearable
+					prefixIcon="lock" shape="circle" prefixIconStyle="font-size:30px"></u-input>
+				</u-form-item>
+			</u-form>
+			
+			<u-button text="登 录" type="primary" shape="circle" size="large" customStyle="font-size:30px"></u-button>
+			
+			<div class="btnBar">
+				<span>
+					<u-text type="primary" text="戳我立即注册>>" size="18" @click="toRegister()"></u-text>
+				</span>
+			</div>
+		</div>
+	</view>
+</template>
+
+<script setup>
+import { reactive, ref } from 'vue';
+ 
+let LoginFormRef = ref()
+
+const user = reactive({
+	contact:'',
+	password:''
+})
+
+const loginRules = {
+	contact:{
+		type:'string',
+		required:true,
+		message:'手机号不能为空',
+		trigger: ['blur']
+	},
+	password:{
+		type:'string',
+		required:true,
+		message:'请填写密码',
+		trigger: ['blur']
+	}
+}
+
+const toRegister = () => {
+	uni.navigateTo({
+		url:'/pages/Register/Register'
+	})
+}
+
+const toHome = () => {
+	uni.navigateTo({
+		url:'/pages/Home/Home'
+	})
+}
+</script>
+
+<style lang="scss">
+	.container{
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		padding: 0 40rpx;
+		height: 80%;
+	}
+	
+	.logincard{
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		width: 100%;
+		gap: 60rpx;
+	}
+	
+	.btnBar{
+		display: flex;
+		flex-direction: row-reverse;
+	}
+</style>
---
+++ Client/forestage/Parcelhub/pages/Register/Register.vue
@@ -0,0 +1,94 @@
+<template>
+	<view class="container">
+		<div class="logincard">
+			<u-text text="注册" bold="true" size="35"></u-text>
+			<u-form :model="user" :rules="RegisterRules" ref="RegisterFormRef">
+				<u-form-item prop="contact">
+					<u-input v-model="user.contact" placeholder="请输入手机号" clearable prefixIcon="phone" shape="circle"
+						prefixIconStyle="font-size:30px"></u-input>
+				</u-form-item>
+
+				<u-form-item prop="password">
+					<u-input v-model="user.password" placeholder="请输入密码" clearable prefixIcon="lock" shape="circle"
+						prefixIconStyle="font-size:30px"></u-input>
+				</u-form-item>
+
+				<u-form-item prop="conf">
+					<u-input v-model="user.conf" placeholder="请输入确认密码" clearable prefixIcon="lock-fill" shape="circle"
+						prefixIconStyle="font-size:30px"></u-input>
+				</u-form-item>
+			</u-form>
+
+			<u-button text="注 册" type="primary" shape="circle" size="large"></u-button>
+
+		</div>
+	</view>
+</template>
+
+<script setup>
+	import {
+		reactive,
+		ref
+	} from 'vue';
+
+	let RegisterFormRef = ref()
+
+	const user = reactive({
+		contact: '',
+		password: '',
+		conf: ''
+	})
+
+	const RegisterRules = {
+		contact: {
+			type: 'string',
+			required: true,
+			message: '手机号不能为空',
+			trigger: ['blur']
+		},
+		password: {
+			type: 'string',
+			required: true,
+			message: '请填写密码',
+			trigger: ['blur']
+		},
+		conf: [{
+				type: 'string',
+				required: true,
+				message: '请填写确认密码',
+				trigger: ['blur']
+			},
+			{
+				validator: (rule, value, callback) => {
+					if(value != user.password) return false
+					return true
+				},
+				message:'两次输入的密码不一致',
+				trigger: ['blur']
+			}
+		]
+	}
+</script>
+
+<style lang="scss">
+	.container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		padding: 0 40rpx;
+		height: 80%;
+	}
+
+	.logincard {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		width: 100%;
+		gap: 60rpx;
+	}
+
+	.btnBar {
+		display: flex;
+		flex-direction: row-reverse;
+	}
+</style>
