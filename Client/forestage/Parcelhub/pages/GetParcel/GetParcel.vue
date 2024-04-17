8da79821bd8969311908a650339425fc068f02af
Cazacufir <103994575+Cazacufir@users.noreply.github.com>
2024-04-17 18:18:53 +0800

    包裹条形码生成

--- Client/forestage/Parcelhub/pages/GetParcel/GetParcel.vue
+++ Client/forestage/Parcelhub/pages/GetParcel/GetParcel.vue
@@ -8,9 +8,15 @@
 
 			<div class="parcelBody" v-for="(child,num) in item.parcel" :key="num">
 				<div class="parcelChild">
-					<u-text :text="child.code" bold="true"></u-text>
-					<u-text :text="'手机号 ' + child.receiveContact + ' 的包裹'" size="13"></u-text>
-					<u-text :text="child.arrivalTime + ' 入站'" size="11"></u-text>
+					<div class="info">
+						<u-text :text="child.code" bold="true"></u-text>
+						<u-text :text="'手机号 ' + child.receiveContact + ' 的包裹'" size="13"></u-text>
+						<u-text :text="child.arrivalTime + ' 入站'" size="11"></u-text>
+					</div>
+
+					<rudon-rowMenuDotDotDot :localdata="options" @change="menuAction($event,child.parcelId)" >
+						<u-icon name="more-dot-fill"></u-icon>
+					</rudon-rowMenuDotDotDot>
 				</div>
 			</div>
 
@@ -19,6 +25,11 @@
 			</div>
 		</div>
 
+		<u-modal :show="isShowMoal" title="包裹条形码" closeOnClickOverlay="true" @confirm="isShowMoal = false" @close="isShowMoal = false">
+			<view class="slot-content">
+				<canvas canvas-id="barcode" class="code"></canvas>
+			</view>
+		</u-modal>
 	</div>
 </template>
 
@@ -32,6 +43,7 @@
 	import {
 		onLoad
 	} from '@dcloudio/uni-app'
+	import wxbarcode from 'wxbarcode'
 
 	// const list = ref([{
 	// 		hub_name: '桂电花江校区食堂菜鸟驿站',
@@ -57,7 +69,7 @@
 	// 	}
 	// ])
 	const list = ref([])
-	
+
 	onLoad(() => {
 		uni.getStorage({
 			key: 'user',
@@ -65,7 +77,7 @@
 				const parcel = {}
 				parcel.receiveName = res.data.name
 				parcel.receiveContact = res.data.contact
-				
+
 				await api.getReceivedParcel(parcel)
 					.then(res => {
 						list.value = [...res.data]
@@ -74,6 +86,34 @@
 			}
 		})
 	})
+
+	const options = [{
+			text: '查看条形码',
+			value: 0
+		},
+		{
+			text: '延迟取件',
+			value: 1
+		},
+		{
+			text: '回退包裹',
+			value: 2
+		}
+	]
+
+	let isShowMoal = ref(false)
+	let currentVal = ref()
+
+	const menuAction = (action, rowId) => {
+		if(action === '') return
+		console.log('action', action)
+		console.log('rowId', rowId)
+		if(action == 0){
+			isShowMoal.value = true
+			currentVal.value = rowId
+			wxbarcode.barcode('barcode', rowId, 200, 80)
+		}
+	}
 </script>
 
 <style scoped lang="scss">
@@ -107,9 +147,8 @@
 
 	.parcelChild {
 		display: flex;
-		flex-direction: column;
 		gap: 10rpx;
-		justify-content: center;
+		justify-content: space-between;
 		margin-top: 20rpx;
 		border-radius: 40rpx;
 		padding: 20rpx;
@@ -122,5 +161,20 @@
 		width: 30%;
 		margin-left: auto;
 		margin-top: 20rpx;
+	}
+
+	.info {
+		display: flex;
+		flex-direction: column;
+		gap: 10rpx;
+		justify-content: center;
+	}
+	
+	.code{
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 80rpx;
+		width: 200rpx;
 	}
 </style>
